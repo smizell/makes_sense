a6b5140c38cfaf1bed3943058ec3c8db80239039
Stephen Mizell <smizell@gmail.com>
2023-04-23 09:33:17 -0500

    Initial commit

---
+++ lib/makes_sense.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "makes_sense/version"
+
+module MakesSense
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/makes_sense_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MakesSense do
+  it "has a version number" do
+    expect(MakesSense::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
